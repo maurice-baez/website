7ce664d3a574da41f08b50d160e1415832aecae3
Maurice <maurice.baez1@gmail.com>
2022-06-21 11:02:36 -0700

    changed image format, added thank you page styling, fixed email form submission

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 document.querySelector(".header__moon").onclick = function () {
   document.querySelector("body").classList.toggle("is-black");
+  //   localStorage.setItem("theme", "dark");
 };
 
 document.querySelector(".header__mob-menu").onclick = function () {
   document.querySelector(".header-menu").classList.toggle("is-active");
 };
+
+// function rememberDarkMode() {
+//   if (localStorage.theme === "dark") {
+//     document.querySelector("body").classList.toggle("is-black");
+//   }
+// }
